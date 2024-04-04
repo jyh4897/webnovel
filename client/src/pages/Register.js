a8a2b95e9c46d237a0d31db0ff913a72e6f672a8
jyh4897 <jyh4897@gmail.com>
2024-04-04 10:26:16 +0900

    추가

---
+++ client/src/pages/Register.js
@@ -0,0 +1,22 @@
+
+
+
+const Register = () => {
+
+
+    return (
+        <div>
+            <div>
+                <p>작품명</p>
+                <div>
+                    <input type="text" />
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Register;
