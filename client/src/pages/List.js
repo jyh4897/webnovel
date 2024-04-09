6371a6cec89254579e56ede341724640b684c38f
jyh4897 <jyh4897@gmail.com>
2024-04-09 10:19:27 +0900

    페이지네이션

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import Paging from '../components/Paging';
 
 
 
@@ -22,32 +23,55 @@ const List = () => {
         title: '',
         avgrate : ''
     })
+    const [count, setCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentPosts, setCurrentPosts] = useState(0);
+    const postPerPage = 5;
+    const indexOfLastPost = page * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage
 
     useEffect(() => {
         async function fetchData() {
-            const rawData = await axios.get('http://localhost:8000/novel', {});
-            const sortedData = await rawData.data.filter((it) => it.category == genre);
-            const novels = sortedData.map((it) => ({
-                novelid : it.novelid,
-                title : it.title,
-                category : it.category,
-                thumbnail : it.thumbnail,
-            }));
-            const rawRate = await axios.get('http://localhost:8000/avgrate', {});
-            const rates = await rawRate.data.filter((it) => ({
-                novelid : it.novelid,
-                title: it.title,
-                avgrate : it.avgrate
-            }));
-            setNovel(novels);
-            setRate(rates);
+            try{
+                const rawData = await axios.get('http://localhost:8000/novel', {});
+                const sortedData = await rawData.data.filter((it) => it.category == genre);
+                const novels = sortedData.map((it) => ({
+                    novelid : it.novelid,
+                    title : it.title,
+                    category : it.category,
+                    thumbnail : it.thumbnail,
+                }));
+                const sortedNovels = [...novels].sort((a,b) => Number(b.novelid) - Number(a.novelid));
+                const rawRate = await axios.get('http://localhost:8000/avgrate', {});
+                const rates = await rawRate.data.filter((it) => ({
+                    novelid : it.novelid,
+                    title: it.title,
+                    avgrate : it.avgrate
+                }));
+                setNovel(sortedNovels);
+                setRate(rates);
+                setCount(sortedNovels.length); 
+            } catch (error) {
+                console.error('Error fetching data', error);
+            }
+            
         }
         fetchData();
     },[genre])
 
+    useEffect(() => {
+        setCurrentPosts(novel.slice(indexOfFirstPost, indexOfLastPost));
+    }, [novel])
+
+    const handleChangePage = (page) => {
+        setCurrentPage(page)
+    }
+
     return (
         <div>
 
+
+            <Paging page={currentPage} count={count} handleChangePage={handleChangePage} postPerPage={postPerPage} />
         </div>
     )
 }
